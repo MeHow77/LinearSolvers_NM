f9550bdd2e2f43c89d656b985bc210409194b4aa
MeHow77 <michal.hawa@gmail.com>
2019-04-02 22:06:50 +0200

    Gauss Seidel, norm

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import numpy as np
-
-
-
+from operator import sub
 
 def main():
     # Ex. A
     a1 = 13
     a2 = a3 = - 1
-    N = 903
+    N = 12
     f = 1
     A = generateSampleMatrix(a1, a2, a3, N, 1)
     b = generateSampleVector(N, f)
@@ -16,6 +14,7 @@ def main():
     resLimit = 10**-9
 
     roots_x = LinSolver_Jacobi(A, b, N, resLimit)
+    print(roots_x)
 
 
 def generateSampleMatrix(a1, a2, a3, N, f):
@@ -53,11 +52,12 @@ def LinSolver_Jacobi(A, b, N, resLimit):
         return
 
     normRes = 1
-
     #TODO how does variety of init values in x affects no. of iterations?
-    x = [1 for _ in range(N)] # init x
+    x = [0 for _ in range(N)] # init x
 
+    noIter = 0
     while normRes > resLimit:
+        noIter += 1
         x_prev = x.copy()
         for i in range(N):
             sum_1 = 0
@@ -66,13 +66,51 @@ def LinSolver_Jacobi(A, b, N, resLimit):
                 sum_1 += A[i][j] * x_prev[j]
             for j in range(i, N):
                 sum_2 += A[i][j] * x_prev[j]
-        x[i] = (b[i] - sum_1 - sum_2)/A[i][i]
-        normRes = calcNormRes(x, x_prev)
+            x[i] = (b[i] - sum_1 - sum_2)/A[i][i]
+        res = calcRes(A, x, b)
+        normRes = calcNorm(res, 2)
+        print(normRes)
+
+    return x
 
 
-def calcNormRes(vec_x, vec_prevX):
-    normRes_squared = list(map(lambda x1, x2: (x1 - x2) ** 2, vec_x, vec_prevX))
+
+
+def multiplyMatrices(A, B):
+    #it is not multiusable function, it's only for lists as they are
+    C = []
+    #A is NxM; B is MxP
+    n = len(A)
+    p = len(B[0])
+    m = len(B)
+
+    for i in range(m):
+        C.append([])
+
+    for i in range(m):
+        for j in range(p):
+            sum = 0
+            for k in range(m):
+                sum += A[i][k] * B[k][j]
+            C[i].append(sum)
+    return C
+
+def calcRes(A, x, b):
+    # res = y - b; y = A*x
+    if isinstance(x[0],list) == False: #so it's vector
+        tmpArr = []
+        tmpArr.append(x)
+
+    y = multiplyMatrices(A, tmpArr)
+    res = list(map(sub, y[0], b))
+    return res
+
+def calcNorm(res, p):
+    normRes_squared = list(map(lambda x1: x1**p, res))
     normRes_squared = sum(normRes_squared)
-    return np.sqrt(normRes_squared)
+    return np.power(1/p, normRes_squared)
+
+
+
 
 main()
